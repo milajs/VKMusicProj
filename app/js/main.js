c4646eccb324790fe115c4a60698187ee7d67733
LyudmilaP <lpromyslova@mail.ru>
2016-06-17 18:14:01 +0300

    fix: play button fixing

--- app/js/main.js
+++ app/js/main.js
@@ -163,34 +163,48 @@ var VKMusicApp = React.createClass({
 
     getInitialState: function() {
 
-        return {audiolist: this.props.audios,
-                audiourl: "", 
-                playing: true, 
-                value: '▶',
-                played: 0,
-                volume: 0.8};
+        return {
+                    currentAudioIndex: -1,  
+                    audiourl: "", 
+                    playing: false, 
+                    value: '▶',
+                    played: 0,
+                    volume: 0.8
+                };
     },
 
     handleNewRowSubmit: function( newaudios ) {
         this.setState( {audiolist: newaudios} );
     },
 
-    handleNewAudioSubmit: function(url) {
-        console.log('it works!!!!!! KEK ->' + url);
-        this.setState( {audiourl: url, playing: true} );
+    handleNewAudioSubmit: function(url,index) {
+        this.setState( {audiourl: url, playing: true, currentAudioIndex: index} );
+        this.updateValue();
+         console.log('it works!!!!!! KEK ->' + url + "\nindex -> " + this.state.currentAudioIndex);
     },
 
     onProgressHandle: function(event) {
-        console.log('::: progress handle ->' + event.played);
         this.setState({ played: parseFloat(event.played) });
     },
 
     onEndedHandle: function(event) {
-        console.log('::: playing is ended :::');
+
+        this.state.currentAudioIndex += 1;
+
+        var audiolist = this.refs.AudiosList.state.audiolist;
+        var audiomodel = audiolist[this.state.currentAudioIndex];
+
+        this.handleNewAudioSubmit(audiomodel.url,this.state.currentAudioIndex);
+
     },
 
     pause: function() { 
-        this.setState( {playing: !this.state.playing, value: this.state.playing ? '▶' : '||'} );
+        this.setState( {playing: !this.state.playing} );
+        this.updateValue();
+    },
+
+    updateValue: function() { 
+        this.setState( {value: this.state.playing ? '▶' : '||'} );
     },
 
     onSeekChange: function(event) {    
@@ -236,7 +250,7 @@ var VKMusicApp = React.createClass({
 
                         <div className="player-holder">
                             <div className="player-design">
-                                <input type="button" className="stop-btn" onClick={this.pause} value={this.state.value} /> 
+                                <input type="button" className="stop-btn" onClick={this.pause} handleUpdate={this.updateValue} value={this.state.value} /> 
                                 <button className="pervios-audio"> <img className="switch-buttons" src="/media/prev-arrow.png" /> </button>
                                 <button className="next-audio"> <img className="switch-buttons" src="/media/next-arrow.png" /> </button>
                                 <input type='range' className="progress-bar" min={0} max={1} step='any' value={this.state.played} onChange={this.onSeekChange} />
@@ -245,7 +259,13 @@ var VKMusicApp = React.createClass({
                             </div>
                         </div>
                 
-                        <AudiosList handleUpdate={this.handleNewAudioSubmit} handleNewAudios={this.handleNewRowSubmit}  />
+                        <AudiosList 
+                            ref='AudiosList'
+                            selectedIndex={this.currentAudioIndex} 
+                            handleUpdate={this.handleNewAudioSubmit} 
+                            handleNewAudios={this.handleNewRowSubmit}  
+                        />
+
                     </div>
                 </div>
             </div>
@@ -258,6 +278,11 @@ var VKMusicApp = React.createClass({
 var AudiosList = React.createClass({
 
     getInitialState:function() {
+        return {audiolist: [],
+                nowPlayingIndex: -1};
+    },
+
+    getDefaultProps:function() {
         return {audiolist: []};
     },
 
@@ -265,22 +290,20 @@ var AudiosList = React.createClass({
 
         vk_getaudios(function (audiosArray){
             if (audiosArray) {
-
                 console.log('alistk ' + audiosArray)
-
                 this.setState( {audiolist: audiosArray} );
             }
-
         }.bind(this))
-
     },
 
     componentDidMount: function() {
         this.loadNewAudiosFromServer();
     },
 
-    handleTest: function(url) {
-        this.props.handleUpdate(url);
+    handleTest: function(url,key) {
+        this.setState( {nowPlayingIndex: key} );
+        this.props.handleUpdate(url,key);
+        console.log('now playing index = ' + this.state.nowPlayingIndex + ' ::::  set key = ' + key);
     },
 
     render: function() {
@@ -295,7 +318,7 @@ var AudiosList = React.createClass({
                 <table className="table">
                   <tbody>
                       {_data.map(function(audioModel, i) {
-                         return <AudioRow audio={audioModel} handleUpdate={this.handleTest} key={i}/>
+                         return <AudioRow audio={audioModel} handleUpdate={this.handleTest} audioIndex={i} key={i} nowPlayingIndex={this.state.nowPlayingIndex} />
                       }.bind(this))}
                   </tbody>
                 </table>
@@ -309,16 +332,36 @@ var AudiosList = React.createClass({
 var AudioRow = React.createClass({
 
     getInitialState: function() {
-        return {value: "▶"};
+        return {value: "▶",
+                selectedClass: "audio-row",
+                lastKey: -1};
+    },
+ 
+    handleClick :function(){
+        this.props.handleUpdate(this.props.audio.url, this.props.audioIndex);
+        this.setState( {playing: !this.state.playing} );
+        this.checkSelected();
     },
 
-    handleClick :function(){
-        this.props.handleUpdate(this.props.audio.url);
+    componentDidMount: function() {
+        this.checkSelected();      
+    },
+
+    checkSelected :function(){
+
+            if (this.props.audioIndex === this.props.nowPlayingIndex) {
+                this.setState( {selectedClass: "audio-row selected"} );
+            } else {
+                this.setState( {selectedClass: "audio-row"} );
+            }
+
+            console.log('::: audioIndex -> ' + this.props.audioIndex + '  ::: currentAudioIndex -> ' + this.props.nowPlayingIndex);
+
     },
 
     render: function() {
         return (
-            <tr className="audio-row">
+            <tr className={this.state.selectedClass}>
                 <td className="artists"> {this.props.audio.artist} </td>
                 <td> {this.props.audio.title} </td>
                 <td className="wrap buttons-onrow">
