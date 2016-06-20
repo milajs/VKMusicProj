4d58c82ab0bcd358d1540e6aeb9dbe583c7e9c23
LyudmilaP <lpromyslova@mail.ru>
2016-06-20 20:14:02 +0300

    update: correct work of progress and volume bars

--- app/components/player/player_body.jsx
+++ app/components/player/player_body.jsx
@@ -9,13 +9,22 @@ class Player extends Component {
 
 		this.state = {
 			ButtonValue: '▶',
-			played: 0
+			played: 0,
+			volume: 0.8
 		};
 	}
 
 	onSeekChange(event) {    
         this.setState({ played: parseFloat(event.target.value) });
         this.refs.player.seekTo(parseFloat(event.target.value));
+    }
+
+    onProgressHandle(event) {
+        this.setState({ played: parseFloat(event.played) });
+    }
+
+    setVolume(event) {   
+        this.setState({ volume: parseFloat(event.target.value) });
     }
 
 	render() {
@@ -28,15 +37,17 @@ class Player extends Component {
 					ref='player'
 					url={this.props.Audiourl} 
 					playing={this.props.playing}
+					volume={this.state.volume}
 					className="player"
-					seekTo={this.props.played} 
+					seekTo={this.props.played}
+					onProgress={this.onProgressHandle.bind(this)}
 				/> 
 
 				<input type="button" className="play-btn-on-player" value={this.state.ButtonValue} />
 				<button className="pervios-audio"> <img className="switch-buttons-pic" src={require('./../media/prev-arrow.png')} /> </button>
 				<button className="next-audio"> <img className="switch-buttons-pic" src={require('./../media/next-arrow.png')} /> </button>
 				<input type='range' className="progress-bar" min={0} max={1} step='any' value={this.state.played} onChange={this.onSeekChange.bind(this)} />
-				<input type='range' className="volume-bar" min={0} max={1} step='any' />
+				<input type='range' className="volume-bar" min={0} max={1} step='any' value={this.state.volume} onChange={this.setVolume.bind(this)} />
 				<img className="volume-pic" src={require('./../media/audio-speaker.png')} />
 			</div>
 
