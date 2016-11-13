2ad2a3389e7d2f44b8f94e871b5c120c4b45cb85
Lyudmila Promyslova <lpromyslova@mail.ru>
2016-11-13 21:22:01 +0300

    new: create empty store

--- app/index.js
+++ app/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/app';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import configureStore from './store/configureStore'
 
 VK.init({
     apiId: 5505191
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
