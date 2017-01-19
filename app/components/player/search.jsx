b4b110f9da861c362ab1c05ae547d895fb03511e
Lyudmila Promyslova <lpromyslova@mail.ru>
2017-01-20 00:01:59 +0300

    Update search.jsx

--- app/components/player/search.jsx
+++ app/components/player/search.jsx
@@ -3,26 +3,26 @@ import React, { Component, PropTypes } from 'react';
 var css = require('../../styles/player.styl');
 
 class SearchField extends Component {
-	handleInput(e) {
+  handleInput(e) {
     if (e.target.value === ``) {
       this.props.onLoadAudios()
     } else {
       this.props.onChange(e.target.value)
     }
-	}
+  }
 
-	render() {
-		return (
-			<div>
-				<input
+  render() {
+    return (
+      <div>
+	<input
           type="text"
           className="search-audio-input"
           placeholder="Search audio"
           onChange={this.handleInput.bind(this)}
         />
-			</div>
-		)
-	}
+      </div>
+    )
+  }
 }
 
 export default SearchField
