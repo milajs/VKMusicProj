7d6c4a7ad77b811c7e7b9e4c22c1103a10926df4
Lyudmila Promyslova <lpromyslova@mail.ru>
2017-01-20 00:06:22 +0300

    Update player.jsx

--- app/components/player/player.jsx
+++ app/components/player/player.jsx
@@ -4,98 +4,98 @@ import ReactPlayer from 'react-player';
 var css = require('../../styles/player.styl');
 
 class Player extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			played: 0,
-			volume: 0.8,
-		};
-	}
+    this.state = {
+      played: 0,
+      volume: 0.8,
+    };
+  }
 
   togglePlay() {
-		this.props.togglePlay();
-	}
+    this.props.togglePlay();
+  }
 
-	changeAudio(direction) {
-		this.props.changeAudio(direction);
-	}
+  changeAudio(direction) {
+    this.props.changeAudio(direction);
+  }
 
-	onSeekChange(e) {
-		this.setState({ played: parseFloat(e.target.value) });
-		this.refs.player.seekTo(parseFloat(e.target.value));
-	}
+  onSeekChange(e) {
+    this.setState({ played: parseFloat(e.target.value) });
+    this.refs.player.seekTo(parseFloat(e.target.value));
+  }
 
-	onProgress(e) {
-		this.setState({ played: parseFloat(e.played) });
-	}
+  onProgress(e) {
+    this.setState({ played: parseFloat(e.played) });
+  }
 
-	setVolume(e) {
-		this.setState({ volume: parseFloat(e.target.value) });
-	}
+  setVolume(e) {
+    this.setState({ volume: parseFloat(e.target.value) });
+  }
 
-	render() {
+  render() {
     const { audioModel = {}, isPlaying } = this.props;
     const ButtonValue = !isPlaying ? '▶' : '||';
 
-		return (
-			<div className="player-body">
-				<ReactPlayer
-					ref='player'
-					url={audioModel.url}
-					playing={isPlaying}
-					volume={this.state.volume}
-					className="player"
-					seekTo={this.props.played}
-					onProgress={this.onProgress.bind(this)}
-					onEnded={this.changeAudio.bind(this, 'next')}
-				/>
+    return (
+      <div className="player-body">
+	<ReactPlayer
+	  ref='player'
+	  url={audioModel.url}
+	  playing={isPlaying}
+	  volume={this.state.volume}
+	  className="player"
+	  seekTo={this.props.played}
+	  onProgress={this.onProgress.bind(this)}
+	  onEnded={this.changeAudio.bind(this, 'next')}
+	/>
 
-				<input
-					type="button"
-					className="play-btn-on-player"
-					value={ButtonValue}
-					onClick={this.togglePlay.bind(this)}
-				/>
+	<input
+	  type="button"
+	  className="play-btn-on-player"
+	  value={ButtonValue}
+	  onClick={this.togglePlay.bind(this)}
+	/>
 
-				<button className="pervios-audio" onClick={this.changeAudio.bind(this, 'prev')}>
+	<button className="pervios-audio" onClick={this.changeAudio.bind(this, 'prev')}>
           <img className="switch-buttons-pic" src={require('../../media/prev-arrow.png')} />
         </button>
 
-				<button className="next-audio" onClick={this.changeAudio.bind(this, 'next')}>
+	<button className="next-audio" onClick={this.changeAudio.bind(this, 'next')}>
           <img className="switch-buttons-pic" src={require('../../media/next-arrow.png')} />
         </button>
 
-				<div className="current-song-on-player">
-					<p className="artist-on-player"> {audioModel.artist} </p>
-					<p className="title-on-player"> - </p>
-					<p className="title-on-player"> {audioModel.title} </p>
-				</div>
+	<div className="current-song-on-player">
+	  <p className="artist-on-player"> {audioModel.artist} </p>
+	  <p className="title-on-player"> - </p>
+	  <p className="title-on-player"> {audioModel.title} </p>
+	</div>
 
-				<input
-					type='range'
-					className="progress-bar"
-					min={0}
-					max={1}
-					step='any'
-					value={this.state.played}
-					onChange={this.onSeekChange.bind(this)}
-				/>
+	<input
+	  type='range'
+	  className="progress-bar"
+	  min={0}
+	  max={1}
+	  step='any'
+	  value={this.state.played}
+	  onChange={this.onSeekChange.bind(this)}
+	/>
 
-				<input
-					type='range'
-					className="volume-bar"
-					min={0}
-					max={1}
-					step='any'
-					value={this.state.volume}
-					onChange={this.setVolume.bind(this)}
-				/>
+	<input
+	  type='range'
+	  className="volume-bar"
+	  min={0}
+	  max={1}
+	  step='any'
+	  value={this.state.volume}
+	  onChange={this.setVolume.bind(this)}
+	/>
 
-				<img className="volume-pic" src={require('../../media/audio-speaker.png')} />
-			</div>
-		)
-	}
+	<img className="volume-pic" src={require('../../media/audio-speaker.png')} />
+      </div>
+    )
+  }
 }
 
 export default Player
