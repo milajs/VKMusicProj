0865dc434b7e29bbd814948371f44c8ec0a415da
Lyudmila Promyslova <lpromyslova@mail.ru>
2017-01-21 00:40:22 +0300

    Update userData.jsx

--- app/components/menu/userData.jsx
+++ app/components/menu/userData.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class UserData extends Component {
-	render() {
+  render() {
     const { data = {} } = this.props;
     const imgUrl = data.photo_200 || require('../../media/no-photo.png');
 
     return (
-			<div>
-				<img className="user-img" src={imgUrl} />
-				<p className="user-name"> {data.first_name} </p>
-				<p className="user-name"> {data.last_name} </p>
-			</div>
-		)
-	}
+	<div>
+	  <img className="user-img" src={imgUrl} />
+	  <p className="user-name"> {data.first_name} </p>
+	  <p className="user-name"> {data.last_name} </p>
+	</div>
+    )
+  }
 }
 
 export default UserData
