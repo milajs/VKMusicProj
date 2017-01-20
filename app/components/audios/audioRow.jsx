8bde4daea658d325a2e6ca10ec39ffd9e1e99528
Lyudmila Promyslova <lpromyslova@mail.ru>
2017-01-21 00:41:57 +0300

    Update audioRow.jsx

--- app/components/audios/audioRow.jsx
+++ app/components/audios/audioRow.jsx
@@ -3,31 +3,30 @@ import React, { Component, PropTypes } from 'react';
 const css = require('../../styles/audioList.styl');
 
 class AudioRow extends Component {
-	handleClick() {
-		this.props.toggleAudio(this.props.audio);
-	}
+  handleClick() {
+    this.props.toggleAudio(this.props.audio);
+  }
 
-	render() {
+  render() {
     const { audio = {}, currentAudio = {}, isPlaying } = this.props;
     const isCurrentAudio = audio.id == currentAudio.id;
 
     const value = (isCurrentAudio && isPlaying) ? '||' : "▶";
+    const selectedClass = isCurrentAudio ? "audio-row selected" : "audio-row";
 
-		const selectedClass = isCurrentAudio ? "audio-row selected" : "audio-row";
-
-		return (
-			<tr className={selectedClass}>
-				<td className="artist"> {audio.artist} </td>
-				<td className="title"> {audio.title} </td>
-				<td className="buttons-onrow">
-					<input type="button" className="stop-btn-onrow" onClick={this.handleClick.bind(this)} value={value} />
-					<a href={audio.url} download="audio.mp3">
-						<button className="download-btn"><img className="download-img" src={require('../../media/download.png')}/></button>
-					</a>
-				</td>
-			</tr>
-		)
-	}
+    return (
+      <tr className={selectedClass}>
+	<td className="artist"> {audio.artist} </td>
+	<td className="title"> {audio.title} </td>
+	<td className="buttons-onrow">
+	  <input type="button" className="stop-btn-onrow" onClick={this.handleClick.bind(this)} value={value} />
+	  <a href={audio.url} download="audio.mp3">
+	    <button className="download-btn"><img className="download-img" src={require('../../media/download.png')}/></button>
+	  </a>
+	</td>
+      </tr>
+    )
+  }
 }
 
 export default AudioRow
